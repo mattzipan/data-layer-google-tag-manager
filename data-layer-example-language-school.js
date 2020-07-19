ec5df36722040d7a5617a2b6f76bd6d07a5f61aa
mattzipan <mattzipan@gmail.com>
2020-07-19 12:37:04 +0200

    first commit

---
+++ data-layer-example-language-school.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [{
+    "language": "de", //Language of UI
+    "sessionDuration": "182", //Session duration in sec
+    "languageIntent": "es", //most-visited course language
+    "pageType": "course page",
+    "courseName": "Spanish A1 - Beginner",
+    "courseLang": "es",
+    "courseLevel": "a1",
+    "courseDuration": "6" //Duration in weeks
+}];
---
+++ data-layer-initialize.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [{
+    "pageCategory": "category page",
+    "pageName": "sneaker overview",
+    "language": "en-US",
+}];
